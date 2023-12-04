3320159517eb8373744fdea4063f80c94569a0dc
Saurabh Pawar <saurbhpawar4545@gmail.com>
2023-12-04 22:26:31 +0530

    first commit

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strict" // treat all js code as newer version
+
+let age = 4
+console.log(age);
+
+console.log(typeof null);
+console.log(typeof undefined);
+s
---
+++ 01_Basics/var.js
@@ -0,0 +1,11 @@
+// const accountId = 1223313
+// let accEmail = 'saurabh@gmail.com'
+// var accPass = 'saurabh123'
+// accCity = "pune"
+// // accountId = 22312
+// console.log(accountId);
+// console.table([accEmail,accPass])
+
+var name = "saurabh"
+
+console.log("name: "+name)
