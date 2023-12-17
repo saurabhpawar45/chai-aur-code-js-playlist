8870156b2b82a7459304e4bc1e770e2436863920
Saurabh Pawar <saurbhpawar4545@gmail.com>
2023-12-18 00:00:53 +0530

    objects

--- 02/objects2.js
+++ 02/objects2.js
@@ -2,10 +2,28 @@
 
 // console.log(newObj);
 
-const obj1 = {a:1,b:2}
-const obj2 = {c:3,d:4}
+// const obj1 = {a:1,b:2}
+// const obj2 = {c:3,d:4}
 
-const obj3 = Object.assign({},obj1,obj2);
+// const obj3 = Object.assign({},obj1,obj2);
 
-console.log(obj3);
-console.log(obj1);
+// console.log(obj3);
+// console.log(obj1);
+
+
+// const obj1 = {a:1,b:2}
+// const obj2 = {c:3,d:4}
+
+// const obj3 = {...obj1,...obj2}
+
+// console.log(obj3);
+// console.log(obj1);
+
+const user = {
+    name : "saurabh",
+    age : 26
+}
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(user.hasOwnProperty('name'))
---
+++ 02/objects3.js
@@ -0,0 +1,11 @@
+const course = {
+    cname : "js in hindi",
+    price : 1000,
+    teacher : "xyz"
+}
+
+const { cname : coursename } = course;
+
+console.log(coursename);
+
+
